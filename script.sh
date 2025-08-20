45e5437dbdada001b03abd7e3ed3e3b3a3efc861
Deepak Mardi <deepakmardi22@gmail.com>
2025-08-20 12:13:21 +0530

    Add script to merge dev into main and push

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to merge dev branch into main and push to remote
+
+set -e
+
+# Checkout main branch
+git checkout main
+
+# Pull latest changes
+git pull origin main
+
+# Merge dev into main
+git merge dev
+
+# Push main to remote
+git push origin main
+
+echo "dev branch has been merged into main and pushed to origin."
